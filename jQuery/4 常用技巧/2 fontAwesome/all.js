9af360596bc4d854494a81491149481947153522
raamiiChu <ericchu0617@gmail.com>
2023-07-16 22:15:59 +0800

    Update: 常用技巧

---
+++ jQuery/4 常用技巧/2 fontAwesome/all.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $("a[id$='word']").addClass("fa-sharp fa-solid fa-file-word");
+    $("a[id$='fb']").addClass("fa-brands fa-facebook");
+    $("a[id$='ig']").addClass("fa-brands fa-instagram");
+});
